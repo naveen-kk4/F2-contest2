acb34a0760fd6aeddeeffd54483dffff112577ac
naveen-kk4 <naveennig2001@gmail.com>
2023-08-21 05:30:34 +0530

    final commit

--- script.js
+++ script.js
@@ -1,45 +1,144 @@
-const students = []; 
-let id = 1;
-  const form1 = document.getElementsByTagName("form")[0];
-  const table = document.querySelector("table > tbody");
-  function deletef(){
-   console.log("I don't exist")
-  } 
+const students = [
+  {
+    ID: 1,
+    name: 'Alice',
+    age: 21,
+    grade: 'A',
+    degree: 'Btech',
+    email: 'alice@example.com'
+  },
+  {
+    ID: 2,
+    name: 'Bob',
+    age: 22,
+    grade: 'B',
+    degree: 'MBA',
+    email: 'bob@example.com'
+  },
+  {
+    ID: 3,
+    name: 'Charlie',
+    age: 20,
+    grade: 'C',
+    degree:'Arts',
+    email: 'charlie@example.com'
+  }
+]; 
+function renderStudents(studentss=students){
+  const tbody = document.getElementById("all-students");
+  tbody.innerHTML = "";
+  studentss.forEach((student)=>{
+    const newRow = document.createElement("tr");
+      const attributes=["ID","name","email","age","grade","degree"];
+      attributes.forEach((attribute)=>{
+        const newCell = document.createElement("td");
+        newCell.innerHTML=student[attribute];
+       if(attribute=="degree")newCell.className="customClass";
+        newRow.appendChild(newCell);
+      });
+      const lastCell = document.createElement("td");
+      const editBtn = document.createElement("button");
+      editBtn.className="customButton";
+      editBtn.style.backgroundImage = "url('./assets/edit1.svg')";
+      editBtn.onclick = () =>editDetails(student.ID);
+      const deleteBtn = document.createElement("button");
+      deleteBtn.className="customButton";
+      deleteBtn.style.backgroundImage = "url('./assets/delete.svg')";
+      deleteBtn.onclick = () =>deleteDetails(student.ID);
+      lastCell.appendChild(editBtn);
+      lastCell.appendChild(deleteBtn);
+      newRow.appendChild(lastCell);
+      tbody.appendChild(newRow);
+
+  });
+ 
+}
+renderStudents();
+function searchStudents(){
+  
+  const searchValue = document.getElementById("search-bar").value.toLowerCase();
+  const arr = students.filter((student)=>{
+    return (
+      student.name.toLowerCase().includes(searchValue) ||
+      student.email.toLowerCase().includes(searchValue) ||
+      student.degree.toLowerCase().includes(searchValue)
+    );
+  });
+  renderStudents(arr);
+
+}
+function deleteDetails(studentId){
+  const confirmation = prompt("If you are sure write 'y' or else write 'n'.");
+  if(confirmation.toLocaleLowerCase()=="y"){
+  let index = students.findIndex((s)=> s.ID == studentId);
+  students.splice(index,1);
+  renderStudents();
+  alert("Deleted Successfully");
+  }
+
+}
+function editDetails(studentId){
+  const student = students.find((s)=> s.ID == studentId);
+  document.getElementById("inp-1").value = student.name;
+  document.getElementById("inp-4").value = student.age;
+  document.getElementById("inp-3").value = student.grade;
+  document.getElementById("inp-5").value = student.degree;
+  document.getElementById("inp-2").value = student.email;
+  document.getElementById("inp-6").value = student.ID;
+  document.getElementById("form-submit").textContent = "Edit Student";
+
+
+}
+
+
+ function handleStudents(event){
+  event.preventDefault();
+ const name = document.getElementById("inp-1").value;
+ const email = document.getElementById("inp-2").value;
+ const age = document.getElementById("inp-4").value;
+ const grade = document.getElementById("inp-3").value;
+ const degree = document.getElementById("inp-5").value;
+const ID = document.getElementById("inp-6").value;
+
+ if(document.getElementById("form-submit").innerText=="Edit Student"){
+  
+    const student = students.find((s)=>s.ID == ID);
+    student.name = name;
+    student.age = age;
+    student.grade = grade ;
+    student.degree=degree;
+    student.email = email;
+    
+
+ }
+ else{
+  
+    const newStudent={
+      ID:students[students.length-1].ID + 1,
+      name,
+      age,
+      grade,
+      degree,
+      email
+    };
+   
+    students.push(newStudent);
+    
+ }
+ document.getElementById("student-form").reset();
+ document.getElementById("form-submit").innerText="Add Student";
+
+ renderStudents();
+
+ 
+
+
+
+}
  
   
   
-  form1.addEventListener("submit",(event)=>{
-    event.preventDefault();
-     let name = document.getElementById("inp-1").value;
-       let email = document.getElementById("inp-2").value;
-       let age = document.getElementById("inp-3").value;
-       let gpa = document.getElementById("inp-4").value;
-       let degree = document.getElementById("inp-5").value;
-      
-        let tableRow = document.createElement("tr");
-       
-      
-       let cell1 = document.createElement("td");
-       cell1.innerText=name;
-       let cell2 = document.createElement("td");
-       cell2.innerText=email;
-       let cell3 = document.createElement("td");
-       cell3.innerText=age;
-       let cell4 = document.createElement("td");
-       cell4.innerText=gpa;
-       let cell5 = document.createElement("td");
-      
-      
-       cell5.innerHTML=degree+"       "+ "<img src = './assets/edit1.svg' alt='an image' />"+'<img  onclick="deletef()"  src = "./assets/delete.svg" alt="an image" />';
-       
-       let cell6 = document.createElement("td");
-       cell6.innerText=id ;
-       tableRow.append(cell6,cell1,cell2,cell3,cell4,cell5);
-       table.appendChild(tableRow);
-       id++;
-   
-       
-  })
+ 
 
 
    
